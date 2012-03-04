2b0e40a6b7c8dd51ddceb057b734b5794f60320e
Carl Lerche <me@carllerche.com>
2012-03-03 22:39:54 -0800

    Add lusty

--- init.el
+++ init.el
@@ -4,15 +4,32 @@
 
 (package-initialize)
 
-(defvar my-packages 
+(defvar my-packages
   '(starter-kit
     starter-kit-lisp
     starter-kit-bindings
     clojure-mode
     clojure-test-mode))
 
+;; Install the listed packages
+
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; Add vendored plugins to load path
+(setq load-path (cons "~/.emacs.d/vendor/lusty-emacs" load-path))
+
+;; Require vendored plugins
+(require 'lusty-explorer)
+(global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
+(global-set-key (kbd "C-x C-b") 'lusty-buffer-explorer)
+
+;; No auth save files kthx
+(setq auto-save-mode nil)
+
+;; Show trailing whitespace
+(setq-default show-trailing-whitespace t)
+
+;; My custom font
 (set-frame-font "Inconsolata-13")
